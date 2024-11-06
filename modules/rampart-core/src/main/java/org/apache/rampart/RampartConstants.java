f8dcc7460487a18382320a0a553781e914fafb31
Robert Lazarski <robertlazarski@gmail.com>
2024-11-06 10:41:26 -1000

    RAMPART-234 Allow custom https listeners to populate the client certificate chain in the message context

--- modules/rampart-core/src/main/java/org/apache/rampart/RampartConstants.java
+++ modules/rampart-core/src/main/java/org/apache/rampart/RampartConstants.java
@@ -5,6 +5,11 @@ public class RampartConstants {
 	public static final String TIME_LOG = "org.apache.rampart.TIME";
 	public static final String MESSAGE_LOG = "org.apache.rampart.MESSAGE";
 	public static final String SEC_FAULT = "SECURITY_VALIDATION_FAILURE";
+        /**
+         * The key under which the HTTPS client certificate, determened by the https listener, may
+         * be populated as a property of the message context.
+         */
+        public static final String HTTPS_CLIENT_CERT_KEY = "https.client.cert.key";
     public static final String MERLIN_CRYPTO_IMPL = "org.apache.ws.security.components.crypto.Merlin";
     public static final String MERLIN_CRYPTO_IMPL_CACHE_KEY = "org.apache.ws.security.crypto.merlin.file";
 
